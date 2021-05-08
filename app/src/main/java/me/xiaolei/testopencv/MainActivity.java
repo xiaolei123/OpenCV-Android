7cd0a2dd97f21c50b306bbae1c3d67a9838b4e45
xiaolei <kavipyouxiang@163.com>
2021-05-08 12:22:54 +0800

    commit

--- app/src/main/java/me/xiaolei/testopencv/MainActivity.java
+++ app/src/main/java/me/xiaolei/testopencv/MainActivity.java
@@ -2,6 +2,8 @@ package me.xiaolei.testopencv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+        }
     }
 
     @Override
