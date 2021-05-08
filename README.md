1901dc369fc731e2eb89cacfbb44a9e5487a5fb1
xiaolei <kavipyouxiang@163.com>
2021-05-08 13:44:08 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OpenCV-Android
 OpenCV-Android
+
+use:
+
+```java
+implementation 'com.github.xiaolei123:OpenCV-Android:4.5.2-2'
+```
--- app/src/main/java/me/xiaolei/testopencv/MainActivity.java
+++ app/src/main/java/me/xiaolei/testopencv/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.opencv.android.JavaCamera2View;
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,19 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.i("XIAOLEI", "未找到内部OPENCV库。使用OPENCV管理器进行初始化");
         } else {
             Log.i("XIAOLEI", "包内包含OpenCV库。使用它！");
         }
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+        }
+
+        JavaCamera2View camera_view = findViewById(R.id.camera_view);
+        camera_view.enableView();
+        camera_view.enableFpsMeter();
+        camera_view.setCameraPermissionGranted();
     }
 }
